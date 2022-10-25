cecf2649352a9aaa2aebc6d07120b5ab81c00720
mykolassl <emykolas@gmail.com>
2022-10-25 19:57:45 +0300

    initial commit

---
+++ app/src/main/java/com/example/bckgrnd/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bckgrnd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_welcome)
+    }
+}
