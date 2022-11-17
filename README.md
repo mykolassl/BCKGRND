4b48794bfd410e9c4ee515b254149916210c83e1
mykolassl <emykolas@gmail.com>
2022-11-17 17:40:14 +0200

    Restrict map to Vilnius area and set max zoom scale

---
+++ README.md
@@ -0,0 +1,17 @@
+# BCKGRND
+
+## What is it?
+
+BCKGRND is an app made for Android, which allows people to search for interesting places to take a photo in Vilnius.
+
+## Tech stack
+
+Front-end
+- Android studio
+- Kotlin
+- ArcGis for map interface
+
+Back-end
+- ASP.NET
+- C#
+- MySQL
--- app/src/main/java/com/example/bckgrnd/MainActivity.kt
+++ app/src/main/java/com/example/bckgrnd/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.example.bckgrnd
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.esri.arcgisruntime.ArcGISRuntimeEnvironment
-import com.esri.arcgisruntime.geometry.Point
+import com.esri.arcgisruntime.concurrent.ListenableFuture
+import com.esri.arcgisruntime.geometry.*
 import com.esri.arcgisruntime.mapping.ArcGISMap
 import com.esri.arcgisruntime.mapping.BasemapStyle
 import com.esri.arcgisruntime.mapping.Viewpoint
+import com.esri.arcgisruntime.mapping.labeling.LabelExpression
+import com.esri.arcgisruntime.mapping.labeling.SimpleLabelExpression
+import com.esri.arcgisruntime.mapping.view.Graphic
+import com.esri.arcgisruntime.mapping.view.GraphicsOverlay
 import com.esri.arcgisruntime.mapping.view.MapView
-import com.esri.arcgisruntime.tasks.geocode.GeocodeParameters
-import com.esri.arcgisruntime.tasks.geocode.LocatorTask
-
+import com.esri.arcgisruntime.symbology.SimpleLineSymbol
+import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol
+import com.esri.arcgisruntime.symbology.TextSymbol
 import com.example.bckgrnd.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     private val activityMainBinding by lazy {
@@ -23,14 +30,35 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding.mapView
     }
 
+    private val envelope = Envelope(
+        Point(25.107056, 54.577634, SpatialReferences.getWgs84()),
+        Point(25.518219, 54.803851, SpatialReferences.getWgs84()))
+
     private fun setApiKeyForApp() {
         ArcGISRuntimeEnvironment.setApiKey(getString(R.string.arcgis_api_key))
     }
 
     private fun setupMap() {
-        val map = ArcGISMap(BasemapStyle.ARCGIS_TOPOGRAPHIC)
+        val map = ArcGISMap(BasemapStyle.ARCGIS_TOPOGRAPHIC).apply {
+            maxExtent = envelope
+            minScale = 200000.0
+        }
+
         mapView.map = map
-        mapView.setViewpoint(Viewpoint(54.687157, 25.279652, 72000.0))
+        mapView.setViewpoint(Viewpoint(envelope))
+    }
+
+    private fun addGraphics() {
+        val graphicsOverlay = GraphicsOverlay()
+        mapView.graphicsOverlays.add(graphicsOverlay)
+
+        val point = Point(25.241123, 54.660329, SpatialReferences.getWgs84())
+        val simpleMarkerSymbol = SimpleMarkerSymbol(SimpleMarkerSymbol.Style.CIRCLE, -0xa8cd, 10f)
+        val blueOutlineSymbol = SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, -0xff9c01, 2f)
+        simpleMarkerSymbol.outline = blueOutlineSymbol
+
+        val pointGraphic = Graphic(point, simpleMarkerSymbol)
+        graphicsOverlay.graphics.add(pointGraphic)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,9 +66,9 @@ class MainActivity : AppCompatActivity() {
 
         setApiKeyForApp()
         setupMap()
+        addGraphics()
 
         setContentView(activityMainBinding.root)
-        //setContentView(R.layout.activity_main)
     }
 
     override fun onPause() {
