ce6236c7cc8a6a760ac886a7a2d7ba9057019389
mykolassl <emykolas@gmail.com>
2022-12-22 15:07:27 +0200

    Added saved places display

--- app/src/main/java/com/example/bckgrnd/SavedPlacesFragment.kt
+++ app/src/main/java/com/example/bckgrnd/SavedPlacesFragment.kt
@@ -8,6 +8,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.beust.klaxon.Klaxon
 import java.io.StringReader
 
@@ -15,7 +17,22 @@ class SavedPlacesFragment : Fragment(R.layout.fragment_saved_places) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val rvPlaces = view.findViewById<RecyclerView>(R.id.rvPlaces)
+
+        val sharedPrefs = requireActivity().applicationContext.getSharedPreferences("savedPlacesJson", Context.MODE_PRIVATE)
+        val savedPlacesJSON = sharedPrefs.getString("savedPlacesJson", "")!!.trimIndent()
+
+        if (savedPlacesJSON.isNotEmpty()) {
+            val parsedPlaces = Klaxon().parseJsonObject(StringReader(savedPlacesJSON))
+            val placesArray = parsedPlaces.array<Place>("data")!!.let { Klaxon().parseFromJsonArray<Place>(it) }
+
+            val adapter = placesArray?.let { VisitedPlaceAdapter(it, requireContext()) }
+            rvPlaces.adapter = adapter
+            rvPlaces.layoutManager = LinearLayoutManager(requireContext())
+        }
+
         // Transition between fragments
         val inflater = TransitionInflater.from(requireContext())
-        enterTransition = inflater.inflateTransition(R.transition.slide_right)    }
+        enterTransition = inflater.inflateTransition(R.transition.slide_right)
+    }
 }
